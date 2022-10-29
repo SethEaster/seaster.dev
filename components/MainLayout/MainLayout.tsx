fbea26afa348b1435518952044b0a6b106ecc4e4
SethEaster <seaster@l1q.pw>
2022-10-29 18:11:43 -0500

    Remove Next.js font optimization
    It looks like you need to fully migrate to Next.js 13's new `app` folder to use this. The feature is experimental and I was having a hard time getting it to build.

--- components/MainLayout/MainLayout.tsx
+++ components/MainLayout/MainLayout.tsx
@@ -1,16 +1,9 @@
-import { Roboto_Condensed } from "@next/font/google";
 import classNames from "classnames";
 import React, { FC } from "react";
 import styles from "./MainLayout.module.css";
 
-const roboto = Roboto_Condensed({ weight: "400" });
-
 export const MainLayout: FC<{ children?: React.ReactNode }> = ({
   children,
 }) => {
-  return (
-    <main className={classNames(styles.main, roboto.className)}>
-      {children}
-    </main>
-  );
+  return <main className={styles.main}>{children}</main>;
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function App({ Component, pageProps, router }: AppProps) {
-  return <Component {...pageProps} key={router.pathname} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} key={router.pathname} />
+    </>
+  );
 }
 
 export default App;
